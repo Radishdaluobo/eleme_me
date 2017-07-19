7001e67e6c44cc632a3b544c615081ca99a10095
Amy <191245518@qq.com>
2017-07-19 19:39:37 +0800

    perfect header module

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,17 +13,21 @@
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
                 <div class="support">
-                    <span class="icon"></span>
-                    <span class="text">{{seller.supports[0].description}}</span>
+                    <span class="icon" :class="classMap[seller.supports[1].type]"></span>
+                    <span class="text">{{seller.supports[1].description}}</span>
                 </div>
             </div>
-            <div class="support-content">
-                <span>{{seller.supports.length}}个</span>
+            <div v-if="seller.supports" class="support-content">
+                <span class="count">{{seller.supports.length}}个</span>
                 <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
         <div class="bulletin-wrapper">
-
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img width="100%" height="100%" :src="seller.avatar">
         </div>
     </div>
 </template>
@@ -35,6 +39,9 @@ export default {
         seller: {
             type: Object
         }
+    },
+    created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
 }
 </script>
@@ -42,14 +49,17 @@ export default {
 //注意css的引入方法
 @import '../../common/scss/mixins';
     .header{
-        background-color:#aaa;
+        color:#fff;
+        position: relative;
+        background:rgba(7,17,27,0.5);
       .content-wrapper{
+          position: relative;
           padding:24px 12px 18px 24px;
         // 两个inline-block之间可能有空隙,是因为有空白字符串,font-size: 0可除去空白,但是切记要将子元素的font-size改回来
           font-size: 0;
           .avatar{
               display:inline-block;
-              /*设置图片和右边文字顶部对其*/
+              /*设置图片和右边文字顶部对齐*/
               vertical-align: top;
               img{
                   border-radius: 2px;
@@ -84,28 +94,94 @@ export default {
               .support{
                   .icon{
                     display: inline-block;
+                    vertical-align: top;
                     width:12px;
                     height: 12px;
                     margin-right: 4px;
                     background-size: 12px 12px;
                     background-repeat: no-repeat;
+                    &.decrease{
+                        @include bg-image('decrease_1')
+                    }
+                    &.discount{
+                        @include bg-image('discount_1') 
+                    }
+                    &.guarantee{
+                        @include bg-image('guarantee_1')
+                    }
+                    &.invoice{
+                        @include bg-image('invoice_1') 
+                    }
                   }
-
-                //   &.decrease{
-                //      @include bg-image('decrease_1')
-                //   }
-                //   &.discount{
-                //      @include bg-image('discount_1') 
-                //   }
-                //   &.guarance{
-                //      @include bg-image('guarance_1')
-                //   }
-                //   &.invpice{
-                //      @include bg-image('invpice_1') 
-                //   }
+                  .text{
+                    font-size: 10px;
+                    line-height: 12px;                  
+                  }
               }
           }
-      }  
+          .support-content{
+              position: absolute;
+              right:12px;
+              bottom:14px;
+              padding: 0 8px;
+              height: 24px;
+              line-height: 24px;
+              border-radius: 14px;
+              background: rgba(0, 0, 0, 0.2);
+              text-align: center;
+              .count{
+                  font-size: 10px;
+                  vertical-align: top;
+              }
+              .icon-keyboard_arrow_right{
+                  vertical-align: top;
+                  font-size: 10px;
+                  margin-left:2px;
+                  line-height: 24px;
+              }
+          }
+      } 
+      .bulletin-wrapper{
+          position: relative;
+          height:28px;
+          line-height: 28px;
+          padding: 0 22px 0 12px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          background: rgba(7,17,27,0.2);
+          .bulletin-title{
+              display: inline-block;
+              vertical-align: top;
+              margin-top:7px;
+              width:22px;
+              height:12px;
+              @include bg-image('bulletin');
+              background-size:22px 12px;
+              background-repeat: no-repeat;
+          }
+          .bulletin-text{
+             vertical-align: top;
+             margin:0 6px;
+             font-size: 10px; 
+          }
+          .icon-keyboard_arrow_right{
+              position: absolute;
+              font-size: 10px;
+              top:7px;
+              right:4px;          
+        }
+      } 
+      .background{
+          position:absolute;
+          top: 0;
+          left: 0;
+          width:100%;
+          height:100%;
+          z-index: -1;
+          // 添加高斯模糊效果
+          filter:blur(10px);
+      }
     }
 </style>
 
